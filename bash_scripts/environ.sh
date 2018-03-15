877617903db8635a0d4bf6c9499f173c8fabc78b
Adam <lefevrad39531@gmail.com>
2018-03-15 16:07:31 +0000

    fixed gitignore

--- bash_scripts/environ.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#sets up environment variables for db loader
-
-export DB='cbbData'
-export USR='adamtlefevre'
-export PW='Snoi7918a.l.0202'
-export ENDPNT='cbbdatabucket.ctixyehceu4w.us-east-2.rds.amazonaws.com'
-export GMAPS='AIzaSyBNaH4Fw9zR8X12rc89FfhxRnlafhkQSgg'
